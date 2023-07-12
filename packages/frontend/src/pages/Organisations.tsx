4a4df1a9063a473c20b4fed349bfcbf939bc5b29
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2023-07-12 20:02:10 +0300

    Organisations page wrap-up

--- packages/frontend/src/pages/Organisations.tsx
+++ packages/frontend/src/pages/Organisations.tsx
@@ -1,7 +1,48 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { useContractRead } from "wagmi";
 import OrganisationDialog from "../components/Organisation/OrganisationDialog";
-import { mockOrganisations } from "../mock/Organisations";
-
+import ABI from "../contracts/ABI";
+import ADDRESS from "../contracts/Address";
+import Spinner from "../utils/Spinner";
 const OrganisationsPage = () => {
+	const { data, isError, isLoading } = useContractRead({
+		address: ADDRESS,
+		abi: ABI,
+		functionName: "getOrganisations",
+	});
+
+	if (isLoading) {
+		return (
+			<div className="p-4 my-auto flex gap-2 justify-center text-center">
+				<Spinner color="info" />
+				<p className="my-auto text-ßxl">Loading Organisations</p>
+			</div>
+		);
+	}
+	if (!isError) {
+		return (
+			<div className="p-4">
+				<div className="alert alert-error">
+					<div className="justify-between flex w-full">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="stroke-current shrink-0 h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+						<span>Couldn't load organisations</span>
+					</div>
+				</div>
+			</div>
+		);
+	}
 	return (
 		<div className="p-4">
 			<div className="max-w-5xl mx-auto">
@@ -19,12 +60,16 @@ const OrganisationsPage = () => {
 						</tr>
 					</thead>
 					<tbody>
-						{mockOrganisations.map((organisation, index) => (
-							<tr key={index} className="bg-white shadow-md hover:bg-gray-50">
-								<td className="border px-4 py-2">{index + 1}</td>
-								<td className="border px-4 py-2">{organisation.name}</td>
-							</tr>
-						))}
+						{//@ts-ignore
+						data.map((org, i) => {
+							console.log(org);
+							return (
+								<tr key={i} className="bg-white shadow-md hover:bg-gray-50">
+									<td className="border px-4 py-2">{i + 1}</td>
+									<td className="border px-4 py-2">{org.name}</td>
+								</tr>
+							);
+						})}
 					</tbody>
 				</table>
 			</div>
--- packages/frontend/src/types/Organisation.ts
+++ packages/frontend/src/types/Organisation.ts
@@ -1,3 +1,11 @@
 export interface Organisation {
+	JSONIPFS?: string;
+	NVT?: bigint;
+	NVTHistorical?: bigint;
+	PVT?: bigint;
+	PVTHistorical?: bigint;
+	klerosAddress?: string;
 	name: string;
+	orgGuid?: string;
+	payoutWallet?: string;
 }
