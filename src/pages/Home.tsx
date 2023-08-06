3fd278f763212de6e1e3bfc53d10ea7a74491144
Mars Notahacker <marsXrobertson@gmail.com>
2023-08-06 18:09:46 +0100

    Adding logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import logo from "../assets/basex-logo.png";
+
 const HomePage = () => {
 	return (
 		<div className="text-center my-4">
-			<h1 className="my-3 text-3xl font-bold">Welcome to BaseX</h1>
+			<h1 className="my-3 text-3xl font-bold">Welcome to &nbsp; <img src={logo} className="inline-block h-14 -mt-1"></img></h1>
 			<p className="my-5 text-lg">
 				BaseX is a revolutionary evaluation system that brings scalability and
 				trustlessness to the forefront. Utilizing the power of Kleros jurors and
