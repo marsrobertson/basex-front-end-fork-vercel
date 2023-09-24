e090b270436bad96ae723aae267bf1041b33925c
Mars Notahacker <marsXrobertson@gmail.com>
2023-09-24 19:00:32 +0100

    Adding Link router react

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,6 +4,7 @@ import { useContractRead } from "wagmi";
 import ABI from "../../contracts/ABI";
 import ADDRESS from "../../contracts/Address";
 import Footer from "./Footer";
+import { Link } from 'react-router-dom';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
 	const getOrganisations = useContractRead({
@@ -48,18 +49,18 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 						</label>
 					</div>
 					<div className="flex-1 px-2 mx-2 text-2xl font-semibold">
-						<a href="/">
+						<Link to="/">
 
 							<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
 							<path d="M 24.960938 2.1015625 A 1.0001 1.0001 0 0 0 24.386719 2.3105469 L 1.3867188 20.210938 A 1.0001 1.0001 0 1 0 2.6132812 21.789062 L 4 20.708984 L 4 48 A 1.0001 1.0001 0 0 0 5 49 L 18.832031 49 A 1.0001 1.0001 0 0 0 19.158203 49 L 30.832031 49 A 1.0001 1.0001 0 0 0 31.158203 49 L 45 49 A 1.0001 1.0001 0 0 0 46 48 L 46 20.708984 L 47.386719 21.789062 A 1.0001 1.0001 0 1 0 48.613281 20.210938 L 25.613281 2.3105469 A 1.0001 1.0001 0 0 0 24.960938 2.1015625 z M 25 4.3671875 L 44 19.154297 L 44 47 L 32 47 L 32 29 A 1.0001 1.0001 0 0 0 31 28 L 19 28 A 1.0001 1.0001 0 0 0 18 29 L 18 47 L 6 47 L 6 19.154297 L 25 4.3671875 z M 20 30 L 30 30 L 30 47 L 20 47 L 20 30 z"></path>
 							</svg>
 
-						</a>
+						</Link>
 					</div>
 					<div className="flex-none hidden lg:block">
 						<ul className="menu menu-horizontal">
 							<li className="flex justify-center gap-1">
-								<a href="/organisations">
+								<Link to="/organisations">
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
 										width="24"
@@ -70,10 +71,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 										<path d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z"></path>
 									</svg>
 									Organisations
-								</a>
+								</Link>
 							</li>
 							<li className="flex justify-center gap-1">
-								<a href="/reports">
+								<Link to="/reports">
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
 										width="24"
@@ -84,10 +85,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 										<path d="M213.66,82.34l-56-56A8,8,0,0,0,152,24H56A16,16,0,0,0,40,40V216a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V88A8,8,0,0,0,213.66,82.34ZM160,51.31,188.69,80H160ZM200,216H56V40h88V88a8,8,0,0,0,8,8h48V216Zm-32-80a8,8,0,0,1-8,8H96a8,8,0,0,1,0-16h64A8,8,0,0,1,168,136Zm0,32a8,8,0,0,1-8,8H96a8,8,0,0,1,0-16h64A8,8,0,0,1,168,168Z"></path>
 									</svg>
 									Reports
-								</a>
+								</Link>
 							</li>
 							<li className="flex justify-center gap-1">
-								<a href="/evaluations">
+								<Link to="/evaluations">
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
 										width="24"
@@ -98,7 +99,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 										<path d="M213.66,82.34l-56-56A8,8,0,0,0,152,24H56A16,16,0,0,0,40,40V216a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V88A8,8,0,0,0,213.66,82.34ZM160,51.31,188.69,80H160ZM200,216H112V200h8a8,8,0,0,0,0-16h-8V168h8a8,8,0,0,0,0-16h-8V136h8a8,8,0,0,0,0-16h-8v-8a8,8,0,0,0-16,0v8H88a8,8,0,0,0,0,16h8v16H88a8,8,0,0,0,0,16h8v16H88a8,8,0,0,0,0,16h8v16H56V40h88V88a8,8,0,0,0,8,8h48V216Z"></path>
 									</svg>
 									Evaluations
-								</a>
+								</Link>
 							</li>
 						</ul>
 					</div>
@@ -114,7 +115,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 				<ul className="menu p-4 w-80 h-full bg-base-200">
 					{/* Sidebar content here */}
 					<li className="flex justify-center gap-1">
-						<a href="/organisations">
+						<Link to="/organisations">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								width="32"
@@ -125,10 +126,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 								<path d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z"></path>
 							</svg>
 							Organisations
-						</a>
+						</Link>
 					</li>
 					<li className="flex justify-center gap-1">
-						<a href="/reports">
+						<Link to="/reports">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								width="32"
@@ -139,10 +140,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 								<path d="M213.66,82.34l-56-56A8,8,0,0,0,152,24H56A16,16,0,0,0,40,40V216a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V88A8,8,0,0,0,213.66,82.34ZM160,51.31,188.69,80H160ZM200,216H56V40h88V88a8,8,0,0,0,8,8h48V216Zm-32-80a8,8,0,0,1-8,8H96a8,8,0,0,1,0-16h64A8,8,0,0,1,168,136Zm0,32a8,8,0,0,1-8,8H96a8,8,0,0,1,0-16h64A8,8,0,0,1,168,168Z"></path>
 							</svg>
 							Reports
-						</a>
+						</Link>
 					</li>
 					<li className="flex justify-center gap-1">
-						<a href="/evaluations">
+						<Link to="/evaluations">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								width="32"
@@ -153,7 +154,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 								<path d="M213.66,82.34l-56-56A8,8,0,0,0,152,24H56A16,16,0,0,0,40,40V216a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V88A8,8,0,0,0,213.66,82.34ZM160,51.31,188.69,80H160ZM200,216H112V200h8a8,8,0,0,0,0-16h-8V168h8a8,8,0,0,0,0-16h-8V136h8a8,8,0,0,0,0-16h-8v-8a8,8,0,0,0-16,0v8H88a8,8,0,0,0,0,16h8v16H88a8,8,0,0,0,0,16h8v16H88a8,8,0,0,0,0,16h8v16H56V40h88V88a8,8,0,0,0,8,8h48V216Z"></path>
 							</svg>
 							Evaluations
-						</a>
+						</Link>
 					</li>
 				</ul>
 			</div>
