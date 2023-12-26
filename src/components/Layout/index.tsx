974cfdf264e13294dc7367b70f2bac126364dd5f
Grigore Gabriel Trifan <trifangaby71@protonmail.com>
2023-12-26 18:42:11 +0200

    add sidebar X

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,9 +6,9 @@ import ADDRESS_prod from "../../contracts/Address_prod";
 import ABI_staging from "../../contracts/ABI_staging";
 import ADDRESS_staging from "../../contracts/Address_staging";
 import Footer from "./Footer";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const STAGING = import.meta.env.VITE_STAGING
+const STAGING = import.meta.env.VITE_STAGING;
 const ABI = STAGING ? ABI_staging : ABI_prod;
 const ADDRESS = STAGING ? ADDRESS_staging : ADDRESS_prod;
 
@@ -56,11 +56,16 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 					</div>
 					<div className="flex-1 px-2 mx-2 text-2xl font-semibold">
 						<Link to="/">
-
-							<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
-							<path d="M 24.960938 2.1015625 A 1.0001 1.0001 0 0 0 24.386719 2.3105469 L 1.3867188 20.210938 A 1.0001 1.0001 0 1 0 2.6132812 21.789062 L 4 20.708984 L 4 48 A 1.0001 1.0001 0 0 0 5 49 L 18.832031 49 A 1.0001 1.0001 0 0 0 19.158203 49 L 30.832031 49 A 1.0001 1.0001 0 0 0 31.158203 49 L 45 49 A 1.0001 1.0001 0 0 0 46 48 L 46 20.708984 L 47.386719 21.789062 A 1.0001 1.0001 0 1 0 48.613281 20.210938 L 25.613281 2.3105469 A 1.0001 1.0001 0 0 0 24.960938 2.1015625 z M 25 4.3671875 L 44 19.154297 L 44 47 L 32 47 L 32 29 A 1.0001 1.0001 0 0 0 31 28 L 19 28 A 1.0001 1.0001 0 0 0 18 29 L 18 47 L 6 47 L 6 19.154297 L 25 4.3671875 z M 20 30 L 30 30 L 30 47 L 20 47 L 20 30 z"></path>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								x="0px"
+								y="0px"
+								width="30"
+								height="30"
+								viewBox="0 0 50 50"
+							>
+								<path d="M 24.960938 2.1015625 A 1.0001 1.0001 0 0 0 24.386719 2.3105469 L 1.3867188 20.210938 A 1.0001 1.0001 0 1 0 2.6132812 21.789062 L 4 20.708984 L 4 48 A 1.0001 1.0001 0 0 0 5 49 L 18.832031 49 A 1.0001 1.0001 0 0 0 19.158203 49 L 30.832031 49 A 1.0001 1.0001 0 0 0 31.158203 49 L 45 49 A 1.0001 1.0001 0 0 0 46 48 L 46 20.708984 L 47.386719 21.789062 A 1.0001 1.0001 0 1 0 48.613281 20.210938 L 25.613281 2.3105469 A 1.0001 1.0001 0 0 0 24.960938 2.1015625 z M 25 4.3671875 L 44 19.154297 L 44 47 L 32 47 L 32 29 A 1.0001 1.0001 0 0 0 31 28 L 19 28 A 1.0001 1.0001 0 0 0 18 29 L 18 47 L 6 47 L 6 19.154297 L 25 4.3671875 z M 20 30 L 30 30 L 30 47 L 20 47 L 20 30 z"></path>
 							</svg>
-
 						</Link>
 					</div>
 					<div className="flex-none hidden lg:block">
@@ -118,7 +123,25 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 			</div>
 			<div className="drawer-side">
 				<label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-				<ul className="menu p-4 w-80 h-full bg-base-200">
+				<ul className="menu p-4 pt-12 w-80 h-full bg-base-200 relative">
+					<label htmlFor="my-drawer-3" className="absolute right-2 top-2">
+						<svg
+							fill="none"
+							strokeWidth={1.5}
+							width="32"
+							height="32"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+							aria-hidden="true"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M6 18 18 6M6 6l12 12"
+							/>
+						</svg>
+					</label>
 					{/* Sidebar content here */}
 					<li className="flex justify-center gap-1">
 						<Link to="/organisations">
